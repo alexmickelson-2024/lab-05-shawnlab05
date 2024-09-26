df1fe5c203c81411a015569d6e78686f00ad42e3
Shawn Miner <shawnmin13@gmail.com>
2024-09-26 16:19:52 -0600

    finished lab

--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -42,9 +42,9 @@ namespace MainProgram
             {
                 WriteLine("\n/----------------\\");
                 WriteLine($"|     X wins!    |");
-                WriteLine("\\----------------/");                
+                WriteLine("\\----------------/");
             }
-            else if(winner == 'O')
+            else if (winner == 'O')
             {
                 WriteLine("\n/----------------\\");
                 WriteLine($"|     O wins!    |");
@@ -52,31 +52,53 @@ namespace MainProgram
             }
             else
             {
-                WriteLine("Looks like a draw");                
+                WriteLine("Looks like a draw");
             }
         }
 
         // TODO: write the functions used in main (and any other helper functions you want to use)
 
-        // DisplayBoard
-        /**
-         * displays the tic-tac-toe board
-         * given the contents of the named cells
-         *  a | b | c
-         * ---+---+---
-         *  d | e | f
-         * ---+---+--
-         *  g | h | i
-         */
+       public static void DisplayBoard(char[] board)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"{board[i * 3]}|{board[i * 3 + 1]}|{board[i * 3 + 2]}");
+                Console.WriteLine("");
+            }
+        }
 
-        //GetMove
-        /* Receive the following as parameters: 1) a string to prompt the user for input, 2) a copy of the board
-         *   The user must enter a single character, 'a' through 'i', that's it.
-         *   Verify the cell is valid (e.g. it is in the board, and no one has played there yet).   
-         *   Also, you'll probably have a loop in here in case the user selects a cell that another player 
-         *   already picked.  You'll need to ask again for them to pick another cell.      
-         * Return: the index of the cell the player selected (if they want 'a' you'd return 0)
-        */
+       public static int GetMove(string prompt, char[] board)
+        {
+            Console.Write(prompt);
+            string inputAsStr = Console.ReadLine().ToLower();
+            if (inputAsStr != "a" || inputAsStr != "b" || inputAsStr != "c" || inputAsStr != "d" || inputAsStr != "e" || inputAsStr != "f" || inputAsStr != "g" || inputAsStr != "h" || inputAsStr != "i")
+            {
+                return GetMove(prompt, board);
+            }
+            else
+            {
+                int index = inputAsStr switch
+                {
+                    "a" => 0,
+                    "b" => 1,
+                    "c" => 2,
+                    "d" => 3,
+                    "e" => 4,
+                    "f" => 5,
+                    "g" => 6,
+                    "h" => 7,
+                    "i" => 8
+                };
+                if (board[index] != 'X' || board[index] != 'O')
+                {
+                    return index;
+                }
+                else
+                {
+                    return GetMove(prompt, board);
+                }
+            }
+        }
 
         //HasWinner
         /* given the board,
@@ -86,17 +108,62 @@ namespace MainProgram
         // of any character; consider writing the function 'CellsAreTheSame'
         // described below
 
-        //bool CellsAreTheSame(char a, char b, char c);
-        /*
-         *  returns true if a, b, and c are all the same
-         */
+       public static bool HasWinner(char[] board)
+        {
+            if (CellsAreTheSame(board[0], board[1], board[2]))
+                return true;
 
+            else if (CellsAreTheSame(board[3], board[4], board[5]))
+                return true;
+
+            else if (CellsAreTheSame(board[6], board[7], board[8]))
+                return true;
+
+            else if (CellsAreTheSame(board[0], board[3], board[6]))
+                return true;
+
+            else if (CellsAreTheSame(board[1], board[4], board[7]))
+                return true;
+
+            else if (CellsAreTheSame(board[2], board[5], board[8]))
+                return true;
+
+            else if (CellsAreTheSame(board[0], board[4], board[8]))
+                return true;
+
+            else if (CellsAreTheSame(board[2], board[4], board[6]))
+                return true;
+
+            else
+                return false;
+
+        }
+
+       public static bool CellsAreTheSame(char a, char b, char c)
+        {
+            return a == b && b == c;
+            // if (a == b && b == c)
+            // {
+            //     return true;
+            // }
+            // else
+            // {
+            //     return false;
+            // }
+        }
         //MakeMove
         /* Receive the current player and the board as a parameter.
          * Call GetMove($"Player {currentPlayer}: Where do you want to play?").
          * Update the board at that index with the current player's symbol.
          */
-        
+
+       public static void MakeMove(char currentPlayer, char[] board)
+        {
+            int index = GetMove(($"Player {currentPlayer}: Where do you want to play?"), board);
+            board[index] = currentPlayer;
+        }
+
 
     }
+
 }
--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,10 +1,76 @@
+using System.ComponentModel.DataAnnotations;
+using MainProgram;
+using static MainProgram.Program;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace TestProject;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestHasWinnerVerticle1()
     {
+        char [] board = new char [9] {'X', 'b','c','X','e','f','X','h','i'};
+       MainProgram.Program.HasWinner(board);
+        
+    }
+
+    [Fact]
+    public void TestHasWinnerVerticle2()
+    {
+        
+        char [] board = new char [9] {'a', 'X','c','d','X','f','g','X','i'};
+       MainProgram.Program.HasWinner(board);
+
+    }
+
+    [Fact]
+    public void TestHasWinnerVerticle3()
+    {
+        char [] board = new char [9] {'a', 'b','O','d','e','O','g','h','O'};
+       MainProgram.Program.HasWinner(board);
+        
+
+    }
+
+    [Fact]
+    public void TestHasWinnerHorizontal1()
+    {
+        char [] board = new char [9] {'X', 'X','X','d','e','f','g','h','i'};
+       MainProgram.Program.HasWinner(board);
+        
+
+    }
+    [Fact]
+    public void TestHasWinnerHorizontal2()
+    {
+        
+        char [] board = new char [9] {'a', 'b','c','X','X','X','g','h','i'};
+       MainProgram.Program.HasWinner(board);
+
+    }
+    [Fact]
+    public void TestHasWinnerHorizontal3()
+    {
+        char [] board = new char [9] {'a', 'b','c','d','e','f','X','X','X'};
+       MainProgram.Program.HasWinner(board);
+        
+
+    }
+    [Fact]
+    public void TestHasWinnerDiagonal1()
+    {
+        char [] board = new char [9] {'X', 'b','c','X','e','f','g','h','X'};
+       MainProgram.Program.HasWinner(board);
+        
+
+    }
+    [Fact]
+    public void TestHasWinnerDiagonal2()
+    {
+        
+        char [] board = new char [9] {'a', 'b','O','d','O','f','O','h','i'};
+       MainProgram.Program.HasWinner(board);
 
     }
 }
